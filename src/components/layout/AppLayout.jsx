f6d5a4ac068d7f23e34584d4acf40c0c2f525a4e
Ravi Raj <91177341+Ravi192003Raj@users.noreply.github.com>
2024-07-28 15:09:26 +0530

    Update AppLayout.jsx

--- src/components/layout/AppLayout.jsx
+++ src/components/layout/AppLayout.jsx
@@ -125,7 +125,7 @@ const AppLayout = () => (WrappedComponent) => {
             />
 
             {isLoading ? (
-              </>
+              <></>
             ) : (
               <Drawer open={isMobile} onClose={handleMobileClose}>
                 <ChatList
